ec00ec696034aa8689d58a6e51a7fd970aaec4bc
Ayush Shrivastav <ayushshrivastav575@gmail.com>
2023-06-03 00:16:37 +0530

    Timeline addedd

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -1,18 +1,78 @@
 import React from "react";
 import "./AboutUs.css";
 import img from "../../assets/img/about-page.jpg";
+import logo from "../../assets/img/abt-logo.png"
 const AboutUs = () => {
   return (
-    <div className="abt-title">
-      <div className="abt-item-1">
-        <a className="abt-hover" data-letter="About Us">
-          About Us
-        </a>
+    <>
+      <div className="abt-title">
+        <div className="abt-item-1">
+          <a className="abt-hover" data-letter="About Us">
+            About Us
+          </a>
+        </div>
+        <div className="abt-item-2">
+          <img className="abt-img" src={img} alt="s" />
+        </div>
       </div>
-      <div className="abt-item-2">
-        <img className="abt-img" src={img} alt="s" />
+      <div className="timeline">
+        <div className="container left-container">
+        <img className="abt-logo" src={logo} alt=""/>
+          <div className="text-box">
+            <h2>SwiGer Inauguration</h2>
+            <small>2018-2019</small>
+            <p className="">
+            Indulge in our new restaurant's tantalizing flavors, fresh ingredients, and exceptional service for an unforgettable dining experience.
+            </p>
+            <span className="left-container-arrow"></span>
+          </div>
+        </div>
+        <div className="container right-container">
+        <img className="abt-logo" src={logo} alt=""/>
+          <div className="text-box">
+            <h2>SwiGer Inauguration</h2>
+            <small>2018-2019</small>
+            <p className="">
+            Indulge in our new restaurant's tantalizing flavors, fresh ingredients, and exceptional service for an unforgettable dining experience.
+            </p>
+            <span className="right-container-arrow"></span>
+          </div>
+        </div>
+        <div className="container left-container">
+        <img className="abt-logo" src={logo} alt=""/>
+          <div className="text-box">
+            <h2>SwiGer Inauguration</h2>
+            <small>2018-2019</small>
+            <p className="">
+            Indulge in our new restaurant's tantalizing flavors, fresh ingredients, and exceptional service for an unforgettable dining experience.
+            <span className="left-container-arrow"></span>
+            </p>
+          </div>
+        </div>
+        <div className="container right-container">
+        <img className="abt-logo" src={logo} alt=""/>
+          <div className="text-box">
+            <h2>SwiGer Inauguration</h2>
+            <small>2018-2019</small>
+            <p className="">
+            Indulge in our new restaurant's tantalizing flavors, fresh ingredients, and exceptional service for an unforgettable dining experience.
+            </p>
+            <span className="right-container-arrow"></span>
+          </div>
+        </div>
+        <div className="container left-container">
+        <img className="abt-logo" src={logo} alt=""/>
+          <div className="text-box">
+            <h2>SwiGer Inauguration</h2>
+            <small>2018-2019</small>
+            <p className="">
+            Indulge in our new restaurant's tantalizing flavors, fresh ingredients, and exceptional service for an unforgettable dining experience.
+            </p>
+            <span className="left-container-arrow"></span>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
