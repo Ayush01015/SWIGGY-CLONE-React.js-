ddd11f640c4cf0828dc3f034d042a38e4d93db24
Ayush Shrivastav <ayushshrivastav575@gmail.com>
2023-11-07 15:50:30 +0530

    Toggle Button added to menu

--- src/components/Accordian/Accordian.js
+++ src/components/Accordian/Accordian.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { IMG_CDN_URL } from "../../constants";
 import Skeleton from "@mui/material/Skeleton";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import ExpandLessIcon from "@mui/icons-material/ExpandLess";
 import "./Accordian.css";
 function truncateText(text) {
   if (text) {
@@ -25,64 +27,78 @@ const Section = ({
   category,
   setCategory,
 }) => {
+  const [isVis, setIsVis] = useState(false);
+  const handleShow = () => {
+    setIsVis(!isVis);
+  };
   return (
     <>
       {category !== "" ? (
         <div className="menu-categories">
           <p className="menu-category-name">{category}</p>
-          <button className="menu-categories-btn">SHOW</button>
+          {isVis === false ? (
+            <button onClick={handleShow} className="menu-categories-btn">
+              <ExpandMoreIcon />
+            </button>
+          ) : (
+            <button onClick={handleShow} className="menu-categories-btn">
+              <ExpandLessIcon />
+            </button>
+          )}
         </div>
       ) : null}
-      <div className="menu-section-container">
-        {Object.keys(data).map((key, i) => (
-          <div key={i}>
-            {setCategory(data[key]?.card?.info?.category)}
-            {/* {console.log(data[key]?.card?.info?.category)} */}
-            <div className="menu-section">
-              <div className="menu-section-info">
-                <p>{data[key]?.card?.info?.name}</p>
-                {/* {console.log(data[key]?.card?.info?.price / 100)} */}
-                <p>
-                  ₹
-                  {!isNaN(data[key]?.card?.info?.price)
-                    ? data[key]?.card?.info?.price / 100
-                    : (100 + Math.random() * 350).toFixed(2)
-                    }
-                </p>
-                <p>
-                  ⭐
-                  {data[key]?.card?.info?.ratings?.aggregatedRating?.rating ===
-                  undefined
-                    ? (1 + Math.random() * 4).toFixed(1)
-                    : data[key]?.card?.info?.ratings?.aggregatedRating?.rating}
-                </p>
-                <p>{truncateText(data[key]?.card?.info?.description)}</p>
-              </div>
+      {!isVis && (
+        <div className="menu-section-container">
+          {Object.keys(data).map((key, i) => (
+            <div key={i}>
+              {setCategory(data[key]?.card?.info?.category)}
+              {/* {console.log(data[key]?.card?.info?.category)} */}
+              <div className="menu-section">
+                <div className="menu-section-info">
+                  <p>{data[key]?.card?.info?.name}</p>
+                  {/* {console.log(data[key]?.card?.info?.price / 100)} */}
+                  <p>
+                    ₹
+                    {!isNaN(data[key]?.card?.info?.price)
+                      ? data[key]?.card?.info?.price / 100
+                      : (100 + Math.random() * 350).toFixed(2)}
+                  </p>
+                  <p>
+                    ⭐
+                    {data[key]?.card?.info?.ratings?.aggregatedRating
+                      ?.rating === undefined
+                      ? (1 + Math.random() * 4).toFixed(1)
+                      : data[key]?.card?.info?.ratings?.aggregatedRating
+                          ?.rating}
+                  </p>
+                  <p>{truncateText(data[key]?.card?.info?.description)}</p>
+                </div>
 
-              <div className="menu-section-img">
-                {data[key]?.card?.info?.imageId !== undefined ? (
-                  <img
-                    src={IMG_CDN_URL + data[key]?.card?.info?.imageId}
-                    alt=""
-                  />
-                ) : (
-                  <Skeleton
-                    variant="rounded"
-                    height="17.5vh"
-                    sx={{ borderRadius: "10px" }}
-                  />
-                )}
-                <button
-                  className="add-item-btn-2"
-                  // onClick={()=>addFoodItem("item")}
-                >
-                  ADD
-                </button>
+                <div className="menu-section-img">
+                  {data[key]?.card?.info?.imageId !== undefined ? (
+                    <img
+                      src={IMG_CDN_URL + data[key]?.card?.info?.imageId}
+                      alt=""
+                    />
+                  ) : (
+                    <Skeleton
+                      variant="rounded"
+                      height="17.5vh"
+                      sx={{ borderRadius: "10px" }}
+                    />
+                  )}
+                  <button
+                    className="add-item-btn-2"
+                    // onClick={()=>addFoodItem(item)}
+                  >
+                    ADD
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      )}
     </>
   );
 };
