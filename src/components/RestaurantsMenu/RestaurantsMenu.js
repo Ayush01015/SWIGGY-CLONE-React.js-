359dde25e168210103448a9e0e492b47d4e6c6e1
Ayush Shrivastav <ayushshrivastav575@gmail.com>
2024-02-08 16:45:23 +0530

    fix/restaurantMenu

--- src/components/RestaurantsMenu/RestaurantsMenu.js
+++ src/components/RestaurantsMenu/RestaurantsMenu.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { IMG_CDN_URL } from "../../constants";
 import ShimmerMenu from "../ShimmerMenu";
@@ -21,12 +21,23 @@ const theme = createTheme({
 
 const RestaurantsMenu = () => {
   const { id } = useParams();
-  
+
   const restaurant = useRestaurants(id);
-  const restaunrantItem =
+
+  // console.log("restaurant", restaurant);
+
+  const restaurantItem =
     restaurant?.cards?.[2]?.groupedCard?.cardGroupMap?.REGULAR ||
-    restaurant?.cards?.[3]?.groupedCard?.cardGroupMap?.REGULAR;
-  console.log("RestaurantsMenu JSON", restaunrantItem);
+    restaurant?.cards?.[3]?.groupedCard?.cardGroupMap?.REGULAR ||
+    restaurant?.cards?.[5]?.groupedCard?.cardGroupMap?.REGULAR;
+
+  const headerData =
+    restaurant?.cards?.[0]?.card?.card?.info ||
+    restaurant?.cards?.[2]?.card?.card?.info ||
+    restaurant?.cards?.[5]?.card?.card?.info;
+
+  // console.log("RestaurantsMenu JSON", restaurantItem);
+
   return !restaurant ? (
     <ShimmerMenu />
   ) : (
@@ -36,48 +47,36 @@ const RestaurantsMenu = () => {
           <div className="menu-head">
             <div className="menu-header-img">
               <img
-                src={
-                  IMG_CDN_URL +
-                  restaurant?.cards?.[0]?.card?.card?.info?.cloudinaryImageId
-                }
+                src={IMG_CDN_URL + headerData?.cloudinaryImageId}
                 alt="resImg"
               />
             </div>
             <div className="header-info">
               <div className="menu-head-content">
-                <p className="res-menu-itm res-name">
-                  {restaurant?.cards?.[0]?.card?.card?.info?.name}
-                </p>
+                <p className="res-menu-itm res-name">{headerData?.name}</p>
                 <p className="res-menu-itm">
-                  {restaurant?.cards?.[0]?.card?.card?.info?.cuisines?.join(
-                    ", "
-                  )}
+                  {headerData?.cuisines?.join(", ")}
                 </p>
-                <p className="res-menu-itm">
-                  {restaurant?.cards?.[0]?.card?.card?.info?.areaName}
-                </p>
-                <p className="res-menu-itm">
-                  {restaurant?.cards?.[0]?.card?.card?.info?.city}
-                </p>
+                <p className="res-menu-itm">{headerData?.areaName}</p>
+                <p className="res-menu-itm">{headerData?.city}</p>
               </div>
             </div>
           </div>
           <div className="menu-footer">
             <span className="sub-menu-item">
-              {restaurant?.cards?.[0]?.card?.card?.info?.avgRatingString ===
-              "NEW"
+              {headerData?.avgRatingString === "NEW"
                 ? "No Rating"
-                : restaurant?.cards?.[0]?.card?.card?.info?.avgRatingString}
+                : headerData?.avgRatingString}
               <ThemeProvider theme={theme}>
                 <StarIcon color="primary" className="star-size" />
               </ThemeProvider>
             </span>
             <span className="sub-menu-item">
-              {restaurant?.cards?.[0]?.card?.card?.info?.sla?.deliveryTime} Mins
+              {headerData?.sla?.deliveryTime + " "}
+              Mins
             </span>
             <span className="sub-menu-item">
-              {"Cost for two ₹" +
-                restaurant?.cards?.[0]?.card?.card?.info?.costForTwo / 100}
+              {"Cost for two ₹ " + headerData?.costForTwo / 100}
             </span>
           </div>
         </div>
@@ -85,7 +84,7 @@ const RestaurantsMenu = () => {
       <div className="menu">
         <span className="menu-span">Menu</span>
         <div className="menu-recommended-items">
-          {Object.values(restaunrantItem?.cards).map((item, i) => (
+          {Object.values(restaurantItem?.cards).map((item, i) => (
             <Accordian {...item?.card?.card?.itemCards} key={i} />
           ))}
         </div>
